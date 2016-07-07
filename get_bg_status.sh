7af3ad777ab0016826a0f949b9a1ba5522e0b197
Brian McGehee <doc@tavian.com>
2016-07-07 16:51:53 -0700

    asdf

--- get_bg_status.sh
+++ get_bg_status.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
-json_content=$(curl "https://api.distelli.com/bmcgehee/envs/0green/vars?apiToken=9kpkkouvo7ras8221r25velouviebwqw2is9t")
+json_content=$(curl "https://api.distelli.com/bmcgehee/envs/0green/vars?apiToken=$API_TOKEN")
 
 bg_pos=$(echo "$json_content" | grep -bo '"BG"' |  sed 's/:.*$//')
 bg_value_pos=$((bg_pos + 14))
 bg_value=${json_content:$bg_value_pos:3}
 
-#echo "$bg_value"
+echo $json_content
+echo "$bg_value"
 
 if [ "$bg_value" = "dev" ]; then
   echo "Green is dev, Blue is prod."
